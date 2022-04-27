0a2ad59b7fa99fa590bae224449454055dceba94
Nana Janashia <nn_janashia@yahoo.com>
2022-04-27 09:58:00 +0000

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,7 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
-// with docker-compose: container-name, with K8s: service-name
+// with docker-compose: container-name, with K8s: service-name - test
 let productsEndpoint = process.env.PRODUCTS_SERVICE || 'localhost'
 let shoppingCartEndpoint = process.env.SHOPPING_CART_SERVICE || 'localhost'
 
